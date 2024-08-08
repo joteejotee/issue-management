1de857feba981b93a02b02c4776e8c7bf465bf1f
jo <jotee.jotee@gmail.com>
2024-08-08 20:11:59 +0900

    Update .gitattributes with LF settings

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,25 +1,20 @@
 package com.example.its.web.issue;
 
-import com.example.its.domain.issue.IssueEntity;
+import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
+@RequiredArgsConstructor //finalが付いたフィールドを引数とするコンストラクタを自動生成するアノテ
 public class IssueController {
+    private final IssueService issueService;
 
-    //  GET  /issues
     @GetMapping("/issues")
     public String showList(Model model) {
-        var issueList = List.of(
-            new IssueEntity(1, "概要1", "説明1"),
-            new IssueEntity(2, "概要2", "説明2"),
-            new IssueEntity(3, "概要3", "説明3")
-        );
 
-        model.addAttribute("issueList", issueList);
+        model.addAttribute("issueList", issueService.findAll());
 
         //↓この行にBPはってデバッグし、右クリ→式の評価→model、でmodelに値がちゃんと入ってるのが確認できる。
         // てことはviewに値を渡す準備できてるってこと。
