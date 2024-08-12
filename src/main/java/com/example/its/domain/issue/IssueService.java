d2832d0817c4b09429b1d1d28436bf120748a7ba
jo <jotee.jotee@gmail.com>
2024-08-12 12:17:09 +0900

    バリデーションの実装

--- src/main/java/com/example/its/domain/issue/IssueService.java
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Service
